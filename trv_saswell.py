fca9587c9e84cec40138148a4577a3ec4c670973
MattWestb <49618193+MattWestb@users.noreply.github.com>
2023-01-26 22:33:57 +0100

    Deleting _TZE200_2ekuz3dz

--- trv_saswell.py
+++ trv_saswell.py
@@ -512,7 +512,6 @@ class Saswell_Thermostat_TZE200(TuyaThermostat):
             ("_TZE200_zuhszj9s", "TS0601"),
             ("_TZE200_zr9c0day", "TS0601"),
             ("_TZE200_h4cgnbzg", "TS0601"),
-            ("_TZE200_2ekuz3dz", "TS0601"),
             ("_TZE200_0dvm9mva", "TS0601"),
             ("_TZE200_exfrnlow", "TS0601"),
             ("_TZE200_9m4kmbfu", "TS0601"),
