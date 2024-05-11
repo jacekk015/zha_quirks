ebe1f9858e3dd391863a1ef0ac992a79896157c9
Jacek Kończewski <jk@mikrosap.pl>
2024-05-11 21:19:48 +0200

    Add Celsius/Fahrenheit and Sensitivity

--- ts0601_temphumid.py
+++ ts0601_temphumid.py
@@ -1,5 +1,5 @@
 import datetime
-from typing import Dict
+from typing import Any, Dict, Optional, Union
 
 import zigpy.types as t
 from zhaquirks.const import (
@@ -12,27 +12,28 @@ from zhaquirks.const import (
     SKIP_CONFIGURATION,
 )
 from zhaquirks.tuya import (
+    TUYA_MCU_COMMAND,
     TUYA_SET_TIME,
-    NoManufacturerCluster,
-    TuyaCommand,
-    TuyaEnchantableCluster,
     TuyaLocalCluster,
-    TuyaNewManufCluster,
-    TuyaPowerConfigurationCluster,
     TuyaTimePayload,
 )
-from zhaquirks.tuya.mcu import DPToAttributeMapping, EnchantedDevice, TuyaMCUCluster
+from zhaquirks.tuya.mcu import (
+    DPToAttributeMapping,
+    TuyaClusterData,
+    TuyaMCUCluster,
+    TuyaOnOffNM,
+)
 from zigpy.profiles import zha
 from zigpy.quirks import CustomDevice
 from zigpy.zcl import foundation
 from zigpy.zcl.clusters.general import (
+    AnalogOutput,
     Basic,
     Groups,
     Identify,
     OnOff,
     Ota,
     PowerConfiguration,
-    PowerSource,
     Scenes,
     Time,
 )
@@ -78,14 +79,68 @@ class TuyaPowerConfigurationCluster3AAA(PowerConfiguration, TuyaLocalCluster):
     }
 
 
-# class TuyaSensorManufCluster(TuyaEnchantableCluster, TuyaMCUCluster):
-class TuyaSensorManufCluster(TuyaMCUCluster):
+class TuyaTempSensivity(AnalogOutput, TuyaLocalCluster):
+    """Analog output for temperature sensivity."""
+
+    def __init__(self, *args, **kwargs):
+        """Init."""
+        super().__init__(*args, **kwargs)
+        self._update_attribute(
+            self.attributes_by_name["description"].id, "Temperature sensivity"
+        )
+        self._update_attribute(self.attributes_by_name["max_present_value"].id, 10)
+        self._update_attribute(self.attributes_by_name["min_present_value"].id, 0.5)
+        self._update_attribute(self.attributes_by_name["resolution"].id, 0.5)
+        self._update_attribute(self.attributes_by_name["application_type"].id, 13 << 16)
+        self._update_attribute(self.attributes_by_name["engineering_units"].id, 62)
+
+    async def command(
+        self,
+        command_id: Union[foundation.GeneralCommand, int, t.uint8_t],
+        *args,
+        manufacturer: Optional[Union[int, t.uint16_t]] = None,
+        expect_reply: bool = True,
+        tsn: Optional[Union[int, t.uint8_t]] = None,
+        **kwargs: Any,
+    ):
+        """Override the default Cluster command."""
+        self.debug(
+            "Sending Tuya Cluster Command. Cluster Command is %x, Arguments are %s, %s",
+            command_id,
+            args,
+            kwargs,
+        )
+
+        if command_id in (0x0000, 0x0001, 0x0004):
+            cluster_data = TuyaClusterData(
+                endpoint_id=self.endpoint.endpoint_id,
+                cluster_name=self.ep_attribute,
+                cluster_attr="present_value",
+                attr_value=7,
+                expect_reply=expect_reply,
+                manufacturer=manufacturer,
+            )
+            self.endpoint.device.command_bus.listener_event(
+                TUYA_MCU_COMMAND,
+                cluster_data,
+            )
+            return foundation.GENERAL_COMMANDS[
+                foundation.GeneralCommand.Default_Response
+            ].schema(command_id=command_id, status=foundation.Status.SUCCESS)
+
+        self.warning("Unsupported command_id: %s", command_id)
+        return foundation.GENERAL_COMMANDS[
+            foundation.GeneralCommand.Default_Response
+        ].schema(command_id=command_id, status=foundation.Status.UNSUP_CLUSTER_COMMAND)
+
+
+class TuyaManufCluster(TuyaMCUCluster):
     """Manufacturer Specific Cluster"""
 
     set_time_offset = 1970
     set_time_local_offset = 1970
 
-    server_commands = TuyaNewManufCluster.server_commands.copy()
+    server_commands = TuyaMCUCluster.server_commands.copy()
     server_commands.update(
         {
             TUYA_SET_TIME: foundation.ZCLCommandDef(
@@ -116,21 +171,33 @@ class TuyaSensorManufCluster(TuyaMCUCluster):
             converter=lambda x: x * 2,
         ),
         9: DPToAttributeMapping(
-            TuyaTemperatureMeasurement.ep_attribute,
-            "temp_unit_convert",
-            converter=lambda x: TemperatureUnitConvert(x),
+            TuyaOnOffNM.ep_attribute,
+            "on_off",
+            converter=lambda x: bool(x),
+            dp_converter=lambda x: TemperatureUnitConvert(x),
+        ),
+        # 19: DPToAttributeMapping(
+        #     TuyaTemperatureMeasurement.ep_attribute,
+        #     "temperature_sensitivity",
+        #     converter=lambda x: x / 2,
+        # ),
+        19: DPToAttributeMapping(
+            TuyaTempSensivity.ep_attribute,
+            "present_value",
+            converter=lambda x: x / 2,
         ),
     }
 
     data_point_handlers = {
         1: "_dp_2_attr_update",
         2: "_dp_2_attr_update",
+        4: "_dp_2_attr_update",
         9: "_dp_2_attr_update",
+        19: "_dp_2_attr_update",
     }
 
 
-# class TuyaSensor(EnchantedDevice, CustomDevice):
-class TuyaSensor(CustomDevice):
+class TuyaDevice(CustomDevice):
     signature = {
         #  endpoint=1 profile=260 device_type=81 device_version=0 input_clusters=[0, 4, 5, 61184]
         #  output_clusters=[10, 25]>
@@ -146,7 +213,7 @@ class TuyaSensor(CustomDevice):
                     Basic.cluster_id,
                     Groups.cluster_id,
                     Scenes.cluster_id,
-                    TuyaSensorManufCluster.cluster_id,
+                    TuyaManufCluster.cluster_id,
                 ],
                 OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
             }
@@ -163,10 +230,12 @@ class TuyaSensor(CustomDevice):
                     Basic.cluster_id,
                     Groups.cluster_id,
                     Scenes.cluster_id,
-                    TuyaSensorManufCluster,
+                    TuyaManufCluster,
                     TuyaTemperatureMeasurement,
                     TuyaRelativeHumidity,
                     TuyaPowerConfigurationCluster3AAA,
+                    TuyaOnOffNM,
+                    TuyaTempSensivity,
                 ],
                 OUTPUT_CLUSTERS: [Time.cluster_id, Ota.cluster_id],
             },
